40de574bf21172e4971b615ac495d150f1989815
Thomas Quan <quantrithanh1999@gmail.com>
2021-05-11 23:18:17 -0400

    first content push

---
+++ newtab/newtab.js
@@ -0,0 +1,24 @@
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("min").innerHTML = m;
+  t = setTimeout(function () {
+    startTime();
+  }, 500);
+}
+startTime();
+
+
